a36dadc0851d59856771f6ef38c0086090c11d87
Eka <eka@deadmediafm.org>
2019-07-06 21:09:16 +0700

    Install themes and add to gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {}
+    },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {}
+    }
+  ],
+  siteMetadata: {
+    title: `Ekaʼs Personal Site`
+  }
+};
