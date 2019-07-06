d3dcebfb0861b87f0f16dcc6a2744efa8285a038
Eka <eka@deadmediafm.org>
2019-07-06 23:35:27 +0700

    Modify theme options and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,32 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-notes`,
-      options: {}
+      options: {
+        basePath: `/notes`,
+        homeText: "Home",
+        breadcrumbSeparator: "»"
+      }
     },
     {
       resolve: `gatsby-theme-blog`,
-      options: {}
+      options: {
+        contentPath: `content/writing`
+      }
     }
   ],
   siteMetadata: {
-    title: `Ekaʼs Personal Site`
+    title: `Ekaʼs Personal Site`,
+    author: `Eka (@ekafyi)`,
+    description: `Personal site of Eka, a front-end developer in Indonesia`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/ekafyi`
+      },
+      {
+        name: `github`,
+        url: `https://github.com/ekafyi`
+      }
+    ]
   }
 };
