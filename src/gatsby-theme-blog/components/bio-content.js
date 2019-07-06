5f7c4bdfcfbd22e027b58f7fc7ed8c3951feeccf
Eka <eka@deadmediafm.org>
2019-07-07 01:47:33 +0700

    Shadow bio component

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+export default () => (
+  <Fragment>
+    Personal site of Eka: front-end web developer, competitive napper, and late
+    bloomer.
+  </Fragment>
+);
